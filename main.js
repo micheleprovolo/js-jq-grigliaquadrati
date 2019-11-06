874d57b61665fa68142751869daaf8ee2fb7d6b2
michele <michele.provolo@gmail.com>
2019-11-06 15:24:03 +0100

    creata griglia

---
+++ main.js
@@ -0,0 +1,6 @@
+// DESCRIZIONE:
+//  Creare una griglia formata da 8x8 quadratini tutti bianchi.
+// 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi,
+// gli altri diventano verdi (tutti i rimanennti)
+// Opzionale: Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
+
