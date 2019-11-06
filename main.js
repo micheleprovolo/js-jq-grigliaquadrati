254b2eff38a1384a4cdc35358ba6dbc04ad6b307
michele <michele.provolo@gmail.com>
2019-11-06 15:44:38 +0100

    quadratini rossi ok

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // DESCRIZIONE:
 //  Creare una griglia formata da 8x8 quadratini tutti bianchi.
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi,
-// gli altri diventano verdi (tutti i rimanennti)
+// gli altri diventano verdi (tutti i rimanenti)
 // Opzionale: Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
 
+$("div.red").click(function() {
+    $(this).css("background", "red");
+    });
