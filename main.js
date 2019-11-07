619b764604275bb7bf408aecf7488bd8b351a3a1
michele <michele.provolo@gmail.com>
2019-11-07 08:48:49 +0100

    punteggio ok

--- main.js
+++ main.js
@@ -4,10 +4,31 @@
 // gli altri diventano verdi (tutti i rimanenti)
 // Opzionale: Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
 
-$("div.red").click(function() {
-    $(this).css("background", "red");
-    });
+$( document ).ready(function() {
 
-$("div.green").click(function() {
-    $(this).css("background", "green");
-    });
+// $("div.red").click(function() {
+//     $(this).css("background", "red");
+//     });
+
+// $("div.green").click(function() {
+//     $(this).css("background", "green");
+//     });
+
+var scoreGreen = 0;
+
+var scoreRed = 0;
+
+$( ".block" ).click(function() {
+
+    if ($(this).hasClass("red")) {
+        $(this).css("background", "red");
+        scoreRed++;
+        document.getElementById("count-red").innerHTML = scoreRed;
+    } else {
+        $(this).css("background", "green");
+        scoreGreen++;
+        document.getElementById("count-green").innerHTML = scoreGreen;
+    }
+});
+
+});
